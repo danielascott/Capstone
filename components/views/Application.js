16d3e15e5c6d4963ef0b7aeee81074257afa88d3
Daniel Scott <danielascott@gmail.com>
2024-01-01 17:52:13 -0600

    Add weather information to Application view

--- components/views/Application.js
+++ components/views/Application.js
@@ -70,10 +70,9 @@ export default () => html`
     <div class="right-column">
     <h3>
     Public holidays are:
-
     </h3>
     <h4>
-        api
+    The weather in ${state.weather.city} is ${state.weather.description}. Temperature is ${state.weather.temp}F, and it feels like ${state.weather.feelsLike}F.
     </h4>
     </div>
 </div>
--- index.js
+++ index.js
@@ -40,14 +40,37 @@ router
 router.hooks({
   before: (done, params) => {
     const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
-    
+
     // Add a switch case statement to handle multiple routes
     switch (view) {
       case "Application":
         axios
-          .get(`https://date.nager.at/api/v3/publicholidays/2023/AT${process.env.PUBLIC_HOLIDAY_API}/application`)
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st%20louis`
+          )
           .then(response => {
-            store.Application.application = response.data;
+            const kelvinToFahrenheit = kelvinTemp =>
+              Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
+
+            store.Application.weather = {
+              city: response.data.name,
+              temp: kelvinToFahrenheit(response.data.main.temp),
+              feelsLike: kelvinToFahrenheit(response.data.main.feels_like),
+              description: response.data.weather[0].main
+            };
+            done();
+        })
+        .catch((err) => {
+          console.log(err);
+          done();
+        });
+        break;
+      // Added in Lesson 7.1
+      case "Pizza":
+        axios
+          .get(`${process.env.PIZZA_PLACE_API_URL}/pizzas`)
+          .then(response => {
+            store.Pizza.pizzas = response.data;
             done();
           })
           .catch((error) => {
@@ -60,11 +83,13 @@ router.hooks({
     }
   },
   already: (params) => {
-    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Application";
+    const view = params && params.data && params.data.view ? capitalize(params.data.view) : "Home";
 
     render(store[view]);
   }
 });
+
+
 router
 .on({
   "/": () => render(),
