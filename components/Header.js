3ce8c0c2830136985a63f77eaa0da6fba5e3efd9
Daniel Scott <danielascott@gmail.com>
2024-01-01 17:09:41 -0600

    Update Header.js and index.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import html from "html-literal";
 
 export default (state) => html`
 <header style="text-align: center;">
-   <img src="/Capstone/images/Calendar-Meets.jpeg" alt="Logo">
+   <img src="${Meets}">
    <h1>${state.header}</h1>
 </header>
 `;
