0e54b296f252a96f6d890997d6e07ebe99af452d
Daniel Scott <danielascott@gmail.com>
2024-01-11 19:57:59 -0600

    Add logo to header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,9 @@
 import html from "html-literal";
+import logo from "/images/CalendarMeets.jpeg";
 
 export default (state) => html`
   <header style="text-align: center;">
     <h1>${state.header}</h1>
-    <img src="./Calendar-Meets" alt="Your Image" />
+    <img src="${logo}" alt="Your Image" />
   </header>
 `;
